88a349e3bd3e10311e5afdc9a8ad9e4167ad8f18
PN <ly.ho@student.passerellesnumeriques.org>
2017-03-16 08:48:45 +0700

    da sua file content

--- Content.php
+++ Content.php
@@ -1,13 +1,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <div class="entry-thumbnail">
-                        <?php thachpham_thumbnail('thumbnail'); ?>
+    <div class="entry-thumbnail">
+        <?php thachpham_thumbnail('thumbnail'); ?>
+    </div>
+    <header class="entry-header">
+        <?php thachpham_entry_header(); ?>
+        <?php thachpham_entry_meta() ?>
+    </header>
+        <div class="entry-content">
+            <?php thachpham_entry_content(); ?>
+            <?php ( is_single() ? thachpham_entry_tag() : '' ); ?>
         </div>
-        <header class="entry-header">
-                        <?php thachpham_entry_header(); ?>
-                        <?php thachpham_entry_meta() ?>
-        </header>
-                <div class="entry-content">
-                        <?php thachpham_entry_content(); ?>
-                        <?php ( is_single() ? thachpham_entry_tag() : '' ); ?>
-                </div>
 </article>
