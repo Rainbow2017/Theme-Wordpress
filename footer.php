49688d6f98960e036d7ad0c5be3e3a924680c413
Utle <ut.le121297@gmail.com>
2017-03-16 08:45:31 +0700

    Add files via upload
    footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+</div> <!-- end #container -->
+<footer id="footer">
+	<div class="copyright">
+		&copy; <?php echo date('Y'); ?> - <?php bloginfo('sitename'); ?>
+	</div>
+</footer>
+</div> <!-- end #container -->
+<?php wp_footer(); ?>
+</body>
+</html>
