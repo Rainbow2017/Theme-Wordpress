1552ff456e98bdf461e593d21ca2f5cd853e707b
Phan Thi Thu Ha <ha.phan@student.passerellesnumeriques.org>
2017-03-20 10:00:15 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,47 +1,76 @@
-<?php
-define(THEME_URL, get_stylesheet_directory());
-define('CORE', THEME_URL. /core);
-/*Nhung file core/ init.php*/
-require_once(CORE. /init.php)
-if(!issert($content
-_width)){
-	content_width=620;
+<?php 
+/**
+@ Thiết lập hàm hiển thị logo
+@ thachpham_logo()
+**/
+if ( ! function_exists( 'thachpham_logo' ) ) {
+  function thachpham_logo() {?>
+    <div class="logo">
+ 
+      <div class="site-name">
+        <?php if ( is_home() ) {
+          printf(
+            '<h1><a href="%1$s" title="%2$s">%3$s</a></h1>',
+            get_bloginfo( 'url' ),
+            get_bloginfo( 'description' ),
+            get_bloginfo( 'sitename' )
+          );
+        } else {
+          printf(
+            '</p><a href="%1$s" title="%2$s">%3$s</a></p>',
+            get_bloginfo( 'url' ),
+            get_bloginfo( 'description' ),
+            get_bloginfo( 'sitename' )
+          );
+        } // endif ?>
+      </div>
+      <div class="site-description"><?php bloginfo( 'description' ); ?></div>
+ 
+    </div>
+  <?php }
+}
+ 
+/**
+@ Thiết lập hàm hiển thị menu
+@ thachpham_menu( $slug )
+**/
+if ( ! function_exists( 'thachpham_menu' ) ) {
+  function thachpham_menu( $slug ) {
+    $menu = array(
+      'theme_location' => $slug,
+      'container' => 'nav',
+      'container_class' => $slug,
+    );
+    wp_nav_menu( $menu );
+  }
 }
 
-/**Khai bao chuc nang cua theme**/
-
-if(!function_exists('trang_theme_setup'){
-	$language_folder= THEME_URL. /language;
-	load_theme_textdomain('trang', $language_folder);
-
-	/** Tu dong them link RSS alen <head>**/
-	add_theme_support('automatics-feed-links');
-	/**Them  post thumbnail**/
-	add_theme_support('post-thumbnails');
-	add_theme_support('post-fomats', array('image','video','gallery', 'audio'));
-
-		add_theme_support('title-tag');
-		/**Them custom background **/
-		$default_background= array(
-			'default-color'=>'#00000'
-			);
-		add_theme_support('custom-background', $default_background);
-
-		/**Them menu**/
-		register_nav_menu('primary-menu',__('Primary Menu', 'thachpham'));
-
-		$sidebar= array(
-			'name'=>__('Main sidebar', 'thachpham')
-			'id'=> 'main-sidebar'
-			'description'=>__('Default sidebar')
-			'class'=> 'main-sidebar',
-			'before_title'=>'<h3 class="widgettitle">',
-
-		
-			);
-		register_sidebar($sidebar);
-}
-add_action('init','trang_theme_setup');
+/**
+@ Tạo hàm phân trang cho index, archive.
+@ Hàm này sẽ hiển thị liên kết phân trang theo dạng chữ: Newer Posts & Older Posts
+@ thachpham_pagination()
+**/
+if ( ! function_exists( 'thachpham_pagination' ) ) {
+  function thachpham_pagination() {
+    /*
+     * Không hiển thị phân trang nếu trang đó có ít hơn 2 trang
+     */
+    if ( $GLOBALS['wp_query']->max_num_pages < 2 ) {
+      return '';
+    }
+  ?>
+ 
+  <nav class="pagination" role="navigation">
+    <?php if ( get_next_post_link() ) : ?>
+      <div class="prev"><?php next_posts_link( __('Older Posts', 'thachpham') ); ?></div>
+    <?php endif; ?>
+ 
+    <?php if ( get_previous_post_link() ) : ?>
+      <div class="next"><?php previous_posts_link( __('Newer Posts', 'thachpham') ); ?></div>
+    <?php endif; ?>
+ 
+  </nav><?php
+  }
 }
 /**
 @ Hàm hiển thị ảnh thumbnail của post.
@@ -166,4 +195,84 @@ if ( ! function_exists( 'thachpham_entry_tag' ) ) {
     endif;
   }
 }
+/**
+  @ Load file /core/init.php
+  @ Đây là file cấu hình ban đầu của theme mà sẽ không nên được thay đổi sau này.
+  **/
+ 
+  // require_once( CORE . '/init.php' );
+  /**
+ @ Thiết lập $content_width để khai báo kích thước chiều rộng của nội dung
+ **/
+ if ( ! isset( $content_width ) ) {
+       /*
+        * Nếu biến $content_width chưa có dữ liệu thì gán giá trị cho nó
+        */
+       $content_width = 620;
+  }
+  /**
+  @ Thiết lập các chức năng sẽ được theme hỗ trợ
+  **/
+  if ( ! function_exists( 'thachpham_theme_setup' ) ) {
+        /*
+         * Nếu chưa có hàm thachpham_theme_setup() thì sẽ tạo mới hàm đó
+         */
+        function thachpham_theme_setup() {
+ 
+        }
+        add_action ( 'init', 'thachpham_theme_setup' );
+ 
+  }
+  /*
+* Thiết lập theme có thể dịch được
+*/
+$language_folder = THEME_URL . '/languages';
+load_theme_textdomain( 'thachpham', $language_folder );
+/*
+* Tự chèn RSS Feed links trong <head>
+*/
+add_theme_support( 'automatic-feed-links' );
+/*
+* Thêm chức năng post thumbnail
+*/
+add_theme_support( 'post-thumbnails' );
+/*
+* Thêm chức năng title-tag để tự thêm <title>
+*/
+add_theme_support( 'title-tag' );
+/*
+* Thêm chức năng post format
+*/
+add_theme_support( 'post-formats',
+    array(
+       'image',
+       'video',
+       'gallery',
+       'quote',
+       'link'
+    )
+ );
+/*
+* Thêm chức năng custom background
+*/
+$default_background = array(
+   'default-color' => '#e8e8e8',
+);
+add_theme_support( 'custom-background', $default_background );
+/*
+* Tạo menu cho theme
+*/
+register_nav_menu ( 'primary-menu', __('Primary Menu', 'thachpham') );
+/*
+* Tạo sidebar cho theme
+*/
+$sidebar = array(
+   'name' => __('Main Sidebar', 'thachpham'),
+   'id' => 'main-sidebar',
+   'description' => 'Main sidebar for Thachpham theme',
+   'class' => 'main-sidebar',
+   'before_title' => '<h3 class="widgettitle">',
+   'after_title' => '</h3>'
+);
+register_sidebar( $sidebar );
 ?>
