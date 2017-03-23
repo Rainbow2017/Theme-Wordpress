2a0f61d70c686137d41c6178c9ff03c566fd75ea
pn/trang.nguyen1 <trang.nguyen1@student.passerellesnumeriques.org>
2017-03-23 14:02:41 +0700

    archive

---
+++ archive.php
@@ -0,0 +1,44 @@
+<?php
+
+get_header(); ?>
+
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<?php
+			// Start the Loop.
+			while ( have_posts() ) : the_post();
+
+			
+				get_template_part( 'content', get_post_format() );
+
+			// End the loop.
+			endwhile;
+
+			// Previous/next page navigation.
+			the_posts_pagination( array(
+				'prev_text'          => __( 'Previous page', 'theme_wordpress-master' ),
+				'next_text'          => __( 'Next page', 'theme_wordpress-master' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'theme_wordpress-master' ) . ' </span>',
+			) );
+
+		// If no content, include the "No posts found" template.
+		else :
+			get_template_part( 'content', 'none' );
+
+		endif;
+		?>
+
+		</main><!-- .site-main -->
+	</section><!-- .content-area -->
+	
+<?php get_footer(); ?>
